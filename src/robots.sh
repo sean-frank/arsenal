4944b3a247ad67b9125bf3175be6936718654166
Kevin Haas <kevin.haas96@gmail.com>
2023-07-11 18:35:32 -0400

    Create robots.sh

---
+++ src/robots.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+# robots.sh - Checks for a website's robots.txt file, parsing and outputting its rules.
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <url>"
+    exit 1
+fi
+
+url="$1"
+robots_url="$url/robots.txt"
+
+robots_content=$(curl -sK "$robots_url")
+
+if [ -z "$robots_content" ]; then
+    echo "No robots.txt file found for $url"
+else
+    echo "Robots.txt rules for $url:"
+    echo "$robots_content"
+fi
