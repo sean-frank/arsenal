7e6d9abf2fb0b35b79bc275df19dc425a13972aa
xransum <xransum@users.noreply.github.com>
2023-07-21 22:54:47 -0400

    Resolves #1

--- src/sniper.sh
+++ src/sniper.sh
@@ -380,6 +380,10 @@ for i in "${!args[@]}"; do
         fi
 
         trailing=""
+        if [ "$redirected" = false ]; then
+            datetime=$(date -u +"%Y-%m-%dT%H:%M:%S.000Z")
+            trailing=" at ${YELLOW}${datetime}${RESET}"
+        fi
 
         printf '%s\n' "${leading} ${BOLD}${url}${RESET}${trailing}"
 
